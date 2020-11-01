6314c3979e6b5d94051358ae7e13b5a22d5d0bc1
AbdullahNoori <nooriabdullah86@gmail.com>
2020-11-01 00:06:36 -0700

    updated index.js with io.on

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ http.listen(3000, () => {
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
   });
 });
